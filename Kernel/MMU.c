8411a5f044545955ee017b4db07ab8ab3373f077
fdelgado <fdelgado@itba.edu.ar>
2017-06-25 18:08:29 -0300

    roflmaodule added

--- Kernel/MMU.c
+++ Kernel/MMU.c
@@ -18,18 +18,20 @@ static void * const shellAddress = (void*)0x400000;
 */
 static void * const executableMemoryAdress = (void*)0x400000;
 
-char* moduleNames[] = {"shell", "sampleDataModule", "sampleCodeModule", "hello", "help", "date", "time", "clear",0};
-void * moduleAddresses[] = {0x600000, 0x800000, 0xA00000, 0xC00000, 0xE00000, 0x1000000, 0x1200000, 0x1400000};
+char* moduleNames[] = {"shell", "sampleDataModule", "sampleCodeModule", "hello", "help", "date", "time", "clear", "roflmao",0};
+void * moduleAddresses[] = {0x600000, 0x800000, 0xA00000, 0xC00000, 0xE00000, 0x1000000, 0x1200000, 0x1400000, 0x1600000};
 
 
 void copyAndExectueDefaultModule(){
-	memcpy(executableMemoryAdress, moduleAddresses[0], 0x20000);
+	memcpy(executableMemoryAdress, moduleAddresses[0], 0x200000);
+  //setKernelPresent(0);
 	((EntryPoint)executableMemoryAdress)(0,0);
 
 
 }
 void copyAndExecuteModule(int moduleIndex, int argc, char *argv[]){
 	memcpy(executableMemoryAdress, moduleAddresses[moduleIndex], 0x200000);
+  //setKernelPresent(0);
 	((EntryPoint)executableMemoryAdress)(argc, argv);
 }
 
@@ -46,7 +48,7 @@ void setKernelPresent(int present){
   uint64_t *PD = 0x10000; 
   uint64_t entry= *PD; 
   if(present) 
-    *PD = entry | 0x1; 
+    *PD = entry | 0x8F; 
   else 
     *PD = entry & ~0x1; 
 }
@@ -61,7 +63,7 @@ void changePDEPresent(int entry, int present){
  	uint64_t PDE = *PD;
  
  	if(present)
-  		*PD =  PDE | 0x1; 
+  		*PD =  PDE | 0x8F; 
   	else
   		*PD = PDE & ~0x1;  
 }
@@ -81,12 +83,12 @@ void changePDE(int entry, uint64_t* physAddr, int present){
   uint64_t oldEntry = *PD;
  
   if(present) 
-    *PD = oldEntry & 0xFFFFF | (uint64_t)physAddr & ~0xFFFFF | 0x1; 
+    *PD = oldEntry & 0xFFFFF | (uint64_t)physAddr & ~0xFFFFF | 0x8F; 
   else 
     *PD = oldEntry & 0xFFFFF | (uint64_t)physAddr & ~0xFFFFF & ~0x1; 
 } 
 
-void pageFaultHandler(){ 
+void pageFaultHandler(){
   copyAndExectueDefaultModule();
 } 
 
